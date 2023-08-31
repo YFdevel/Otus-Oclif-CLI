ac301e5423ec7f739bd956107821249144062568
Yuriy <yfrolou@mail.ru>
2023-08-31 11:47:40 +0300

    changed register.ts

--- todolist/src/commands/register.ts
+++ todolist/src/commands/register.ts
@@ -6,7 +6,10 @@ import {connectDb} from '../utils/connect-db.js'
 // eslint-disable-next-line node/no-missing-import
 import UserModel from '../models/user-model.js'
 // eslint-disable-next-line node/no-missing-import
+import {UserModelDto} from '../dto/user-model-dto.js'
+// eslint-disable-next-line node/no-missing-import
 import {Login} from './login.js'
+import {All} from "./all";
 
 interface UserCreateDto {
   firstName: string,
@@ -55,18 +58,27 @@ export class Register extends Command {
     return userCreated
   }
 
-  public async userRegister(): Promise<void> {
+  public async userRegister(): Promise<UserCreateDto> {
     const firstName: string = await ux.prompt('What is your name?')
     const lastName: string = await ux.prompt('What is your lastname?')
     const nickName: string = await ux.prompt('Enter your nickname')
     const password: string = await ux.prompt('Enter your password')
-    await this.createUser({firstName, lastName, nickName, password})
+    const user = await this.createUser({firstName, lastName, nickName, password})
+    return user
   }
 
   public async run(): Promise<void> {
     await connectDb()
-    await this.userRegister()
+    const user = await this.userRegister()
     await this.parse(Register)
-    await Login.run()
+    if (user) {
+      this.log('Account created successfully!')
+      this.log('Enter your nickname and password for starting!')
+      setTimeout(() => {
+        Login.run([])
+      }, 1000)
+    } else {
+      this.log('Oops. An unexpected error occurred ')
+    }
   }
 }
